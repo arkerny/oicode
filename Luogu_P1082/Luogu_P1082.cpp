a42eabb0fcffb0d7b517b56b299654b735b28b17
Hacker_Nick <2025440965@qq.com>
2022-05-12 08:03:20 +0800

    May 12, 2022

--- Luogu_P1082/Luogu_P1082.cpp
+++ Luogu_P1082/Luogu_P1082.cpp
@@ -12,19 +12,26 @@ typedef unsigned long long ull;
 
 const int INF = 0x3f3f3f3f;
 
-ll a, b;
+ll a,b,x,y;
+
+void exgcd(ll a,ll b,ll &x,ll &y)   //扩展欧几里得算法
+{
+    if(b==0)
+    {
+        x=1;
+        y=0;
+        return ;
+    }
+    exgcd(b,a%b,y,x);
+    y-=a/b*x;
+}
 
 int main()
 {
-    cin >> a >> b;
-    for(ll i = 1; i <= 2000000000; i++)
-    {
-        if(a * i % b == 1)
-        {
-            cout << i;
-            return 0;
-        }
-    }
+    cin>>a>>b;
+    ll d=gcd(a,b);
+    
+
 
     return 0;
 }
