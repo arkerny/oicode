071f5687423cbe1ff1cf926fa93c09c32ebb05a4
Hacker_Nick <2025440965@qq.com>
2022-05-21 20:42:28 +0800

    Add FastInit, Pull and Push

---
+++ FastInit.py
@@ -0,0 +1,25 @@
+import os
+
+def Add_URL(URL):
+    os.system("git remote set-url --add all " + URL)
+
+Flag = False
+
+while True:
+    if Flag==False:
+        print("Have you Added Origin 'All' already? (Input Y or N) :")
+        Answer = input()
+        if Answer == "Y":
+            Flag = True
+
+    print("Please Input Origin URL You Want to Add (Input EOF to Exit) :")
+    URL = input()
+
+    if URL == "EOF":
+        break
+    
+    if Flag == False:
+        os.system("git remote add all " + URL)
+        Flag = True
+    else:
+        Add_URL(URL)
