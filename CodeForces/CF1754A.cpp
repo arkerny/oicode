1e6b68b2aaaf9fe3f73cf41ac3ea8bae0de2fa8d
arkerny <arkerny@gmail.com>
2022-10-24 20:40:34 +0800

    Mon Oct 24 20:40 2022

---
+++ CodeForces/CF1754A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define il inline
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define lowbit(_x) (_x&-_x)
+#define fo(_var,_a,_b) for(int _var=_a;_var<=_b;_var++)
+#define fr(_var,_a,_b) for(int _var=_a;_var>=_b;_var--)
+#define foo(_var,_a,_b,_c) for(int _var=_a;_var<=_b;_var+=_c)
+#define frr(_var,_a,_b,_c) for(int _var=_a;_var>=_b;_var-=_c)
+#define clr(_arr,_val) memset(_arr,_val,sizeof(_arr));
+
+int T;
+int n;
+char str[110];
+
+void solve()
+{
+    scanf("%d",&n);
+    scanf("%s",str+1);
+    int Qnum=0,Anum=0;
+    stack<char> s;
+    fo(i,1,n)
+    {
+        if(str[i]=='Q')
+        {
+            s.push(str[i]);
+        }
+        else
+        {
+            if(s.empty())
+            {
+                continue;
+            }
+            s.pop();
+        }
+    }
+    if(s.empty())
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return ;
+}
+
+int main()
+{
+    scanf("%d",&T);
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
